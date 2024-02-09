43b5b179523dd286442d346a90ad52ae841ff4fb
Moudi Kawi <kawimoudi@gmail.com>
2024-02-09 20:33:44 +0000

    create List Page component

---
+++ src/pages/List.jsx
@@ -0,0 +1,12 @@
+function List() {
+    return (
+        <main>
+            <section>
+                <h1>Meteorites</h1>
+                <h3>List View</h3>
+            </section>
+        </main>
+    )
+}
+
+export default List
